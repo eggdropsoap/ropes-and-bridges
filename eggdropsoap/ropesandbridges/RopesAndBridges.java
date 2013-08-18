f1722952ee306d3c5cfdf5398898ec103c3d88c8
eggdropsoap <eggdropsoap@pipemaze.com>
2013-08-17 21:06:45 -0700

    Forge boilerplate

---
+++ eggdropsoap/ropesandbridges/RopesAndBridges.java
@@ -0,0 +1,44 @@
+package eggdropsoap.ropesandbridges;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler; // used in 1.6.2
+//import cpw.mods.fml.common.Mod.PreInit;    // used in 1.5.2
+//import cpw.mods.fml.common.Mod.Init;       // used in 1.5.2
+//import cpw.mods.fml.common.Mod.PostInit;   // used in 1.5.2
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod(modid="RopesAndBridges", name="Ropes and Bridges", version="0.1.0")
+@NetworkMod(clientSideRequired=true, serverSideRequired=false)
+public class RopesAndBridges {
+
+    // The instance of your mod that Forge uses.
+    @Instance("RopesAndBridges")
+    public static RopesAndBridges instance;
+   
+    // Says where the client and server 'proxy' code is loaded.
+    @SidedProxy(clientSide="eggdropsoap.ropesandbridges.client.ClientProxy", serverSide="eggdropsoap.ropesandbridges.CommonProxy")
+    public static CommonProxy proxy;
+   
+    @EventHandler // used in 1.6.2
+    //@PreInit    // used in 1.5.2
+    public void preInit(FMLPreInitializationEvent event) {
+            // Stub Method
+    }
+   
+    @EventHandler // used in 1.6.2
+    //@Init       // used in 1.5.2
+    public void load(FMLInitializationEvent event) {
+            proxy.registerRenderers();
+    }
+   
+    @EventHandler // used in 1.6.2
+    //@PostInit   // used in 1.5.2
+    public void postInit(FMLPostInitializationEvent event) {
+            // Stub Method
+    }
+}
---
+++ eggdropsoap/ropesandbridges/client/ClientProxy.java
@@ -0,0 +1,12 @@
+package eggdropsoap.ropesandbridges.client;
+
+import eggdropsoap.ropesandbridges.CommonProxy;
+import net.minecraftforge.client.MinecraftForgeClient;
+
+public class ClientProxy extends CommonProxy {
+    
+    @Override
+    public void registerRenderers() {
+            // This is for rendering entities and so forth later on
+    }
+}
